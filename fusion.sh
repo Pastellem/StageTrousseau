c564e792d413c68e7acd3a75a1c7ccad614239c0
pastelleM <37301895+Pastellem@users.noreply.github.com>
2019-06-23 22:14:31 +0200

    Update fusion.sh
    doublons

--- fusion.sh
+++ fusion.sh
@@ -1,37 +1,55 @@
  #!/bin/bash +v
 
 #INPUT Variables
-WGS_SNP=$1
-WGS_CNV=$2
-WES_SNP=$3
-WES_CNV=$4
+WGS_un=$1
+WGS_deux=$2
+WGS_CNV=$3
+WES_SNP=$4
+WES_CNV=$5
 
-echo "Cat des WGS"
-cat $WGS_SNP $WGS_CNV |grep -v "^#" > tmpWGS.vcf
+
+echo "Fusion de WGS 1 et 2"
+#detection des mauvaise ligne
+grep -v "^#" $WGS_un > tmp # on retire les ligne meta
+grep -v "^chr" tmp > tmpMauvais
+grep "^chr" $WGS_un > tmpBon
+sed -i "s/^.*/chr&/g" tmpMauvais > tmpCorrige
+cat tmpCorrige tmpBon > tmpWGS1.vcf
+
+grep -v "^#" $WGS_deux > tmp # on retire les ligne meta
+grep -v "^chr" tmp > tmpMauvais
+grep "^chr" $WGS_deux > tmpBon
+sed -i "s/^/chr&/g" tmpMauvais > tmpCorrige
+cat tmpCorrige tmpBon > tmpWGS2.vcf
+echo "Nouveau WGS_SNP"
+cat tmpWGS1.vcf tmpWGS2.vcf > tmpWGS_SNP
+cat tmpWGS_SNP $WGS_CNV |grep -v "^#" > tmpWGS.vcf
+
 
 echo "Cat des WES"
-cat $WES_SNP $WES_CNV |grep -v "#" > tmpWES.vcf
-
+cat $WES_SNP $WES_CNV |grep -v "#" > tmpWES.vcf # on retire les ligne meta
 
 cat tmpWGS.vcf|awk -F "\t" '{print $1"_"$2"_"$3"_"$4"_"$5}' > tmp_Sha_WGS
 cat tmpWES.vcf|awk -F "\t" '{print $1"_"$2"_"$3"_"$4"_"$5}' > tmp_Sha_WES
 
 echo "Search Doublons in file SHA WGS vs WES"
 cat tmp_Sha_WGS tmp_Sha_WES | sort -T tmpSort | uniq -d > tmp_Sha_doublons
+cat tmp_Sha_doublons tmp_Sha_WES | sort -T tmpSort | uniq -u > tmpSHAUniqWES
+cat tmp_Sha_doublons tmp_Sha_WGS | sort -T tmpSort | uniq -u > tmpSHAUniqWGS
 
 sed -i "s/\_/\t/g" tmp_Sha_doublons
-sed -i "s/\_/\t/g" tmp_Sha_WES
-sed -i "s/\_/\t/g" tmp_Sha_WGS
+sed -i "s/\_/\t/g" tmpSHAUniqWES
+sed -i "s/\_/\t/g" tmpSHAUniqWGS
 
 echo "Search Uniq in file WGS vs Doublon"
-grep -v -F -f tmp_Sha_doublons tmpWGS.vcf | sort -T tmpSort | uniq -u > tmpuniqWGS
+grep -F -f tmpSHAUniqWGS tmpWGS.vcf | sort -T tmpSort | uniq -u > tmpuniqWGS
 
 echo "Search Uniq in file Doublon vs WES"
-grep -v -F -f tmp_Sha_doublons tmpWES.vcf | sort -T tmpSort| uniq -u > tmpuniqWES
+grep -F -f tmpSHAUniqWES tmpWES.vcf | sort -T tmpSort| uniq -u > tmpuniqWES
 
-grep -F -f tmp_Sha_doublons tmpWES.vcf > tmDoublonWES
-grep -F -f tmp_Sha_doublons tmpWGS.vcf > tmpDoublouWGS
-cat tmDoublonWES tmpDoublouWGS > tmpdoublon
+grep -F -f tmp_Sha_doublons tmpWES.vcf > tmpDoublonWES
+grep -F -f tmp_Sha_doublons tmpWGS.vcf > tmpDoublonWGS
+cat tmpDoublonWES tmpDoublonWGS > tmpdoublon
 
 echo "Tag absence/presence WGS or WES or both"
 sed -e "s/$/\tType=WGS-WES/g" tmpdoublon > tmpdoublons2
@@ -39,7 +57,7 @@ sed -e "s/$/\tType=WGS-0/g" tmpuniqWGS > tmpuniqWGS2
 sed -e "s/$/\tType=0-WES/g" tmpuniqWES > tmpuniqWES2
 
 echo "Cat final form"
-cat tmpdoublons2 tmpuniqWGS2 tmpuniqWES2 > fichier_name.vcf
+cat tmpdoublons2 tmpuniqWGS2 tmpuniqWES2 > CRO6502.vcf
 
 echo "cleaning tempory file"
 rm tmp*
